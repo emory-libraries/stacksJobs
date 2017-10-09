88240b0806768ae46b65edd6c1e6278069fb2cd5
Alex <alexander.g.cooper@gmail.com>
2017-10-09 12:54:05 -0400

    Create manage_ustorgd_set.sh

---
+++ manage_ustorgd_set.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+config="/sirsi/webserver/config/"
+. ${config}environ     # dot in environ variables
+
+# export all of the environ variables to my children
+for env_var in $(cat ${config}environ | awk -F'=' '{print $1}')
+do
+  export ${env_var}
+done
+
+run_job="/sirsi/webserver/bin/stacks_jobs/run_ustorgd_job.rb"
+ruby ${run_job}
+
+exit 0
