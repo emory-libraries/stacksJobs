e0def4208ee78e73d4f364ead9c32e23e858c091
Alex <alexander.g.cooper@gmail.com>
2018-04-24 19:53:54 -0400

    Adds listener function

--- run_stacks_job.rb
+++ run_stacks_job.rb
@@ -24,26 +24,70 @@ def run_job(url,apikey,values)
 
 end
 
+def parse_xml(xml)
+
+    outcome = 1
+    begin
+        job_id = /Job no\.\ ([0-9]+)\ triggered/.match(xml)
+        response = job_id[1]
+        outcome = 0
+    rescue
+        STDERR.puts 'Could not parse xml'
+        return outcome
+    end
+    return response
+
+end
+
+def listener(job_id)
+
+    outcome = 1
+    begin
+        running = true
+        job_dir = "/sirsi/webserver/integrations/jobs/M0/"
+        webhook = job_dir + job_id
+        while running
+            if not File.file?(webhook)
+                running = true
+            else
+                running = false
+            end
+        end
+        outcome = 0
+    rescue
+        STDERR.puts 'Could not find webkook'
+        return outcome
+    end
+    return outcome
+
+end
+
 # process argument
 input_arg = ARGV[0]
 if input_arg == "univ"
-    job_xml = File.open("files/job_univ.xml")
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_univ.xml")
 elsif input_arg == "bus"
-    job_xml = File.open("files/job_bus.xml")
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_bus.xml")
 elsif input_arg == "chem"
-    job_xml = File.open("files/job_chem.xml")
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_chem.xml")
 elsif input_arg == "hlth"
-    job_xml = File.open("files/job_hlth.xml")
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_hlth.xml")
 elsif input_arg == "lsc"
-    job_xml = File.open("files/job_lsc.xml")
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_lsc.xml")
 elsif input_arg == "musme"
-    job_xml = File.open("files/job_musme.xml")
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_musme.xml")
 elsif input_arg == "wd"
-    job_xml = File.open("files/job_withdraw.xml")
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_withdraw.xml")
+elsif input_arg == "new"
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_new.xml")
+elsif input_arg == "940"
+    job_xml = File.open("/sirsi/webserver/bin/stacks_jobs/files/job_940.xml")
+else
+    STDERR.puts 'Expected input argument but failed'
 end
 # build job xml
 begin
-    setId = File.open("files/setId")
+    setId = File.open("/sirsi/webserver/bin/stacks_jobs/files/setId")
     setId = setId.read
     setId = setId.chomp
     setId = setId.to_s
@@ -54,7 +98,7 @@ rescue
     STDERR.puts 'Could not open and edit job xml'
 end
 begin
-    configuration = File.open("files/run_a_job.cfg")
+    configuration = File.open("/sirsi/webserver/bin/stacks_jobs/files/run_a_job.cfg")
 rescue
     STDERR.puts 'Could not open configuration file'
 end
@@ -80,7 +124,11 @@ else
     url = url
 end
 response,outcome = run_job(url,apikey,values)
-if outcome == 0
+xml = response
+response = parse_xml(xml)
+job_id = response
+response = listener(job_id)
+if response == 0
     STDOUT.puts 'Done'
 else
     STDERR.puts response
